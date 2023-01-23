5d06406596f2702d99abe364006a772d0cfd5072
Noe-Sanchez <danfoot4567@gmail.com>
2023-01-23 06:19:38 +0000

    Working backend primitive requests

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 8004
 
+app.get('/api', (req, res) => {
+  console.log('Inbound API request');
+  res.set({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.json({response: 'Touched the api', data: '420'});
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('Inbound default request');
+  res.send('Debug route')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
